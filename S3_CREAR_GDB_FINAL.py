e019cd39eb7e79201af47cacd37f3699a7e8f395
virulilla <e.salido.castro@gmail.com>
2020-07-27 17:30:05 +0200

    Se comenta la exportacion e importacion del esquema de la gdb nativa. Se eliminan manualmente las entidades para volver a hacer el append posteriormente

--- S3_CREAR_GDB_FINAL.py
+++ S3_CREAR_GDB_FINAL.py
@@ -9,7 +9,8 @@
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 
-import arcpy, os
+import arcpy, os, time
+from datetime import datetime
 
 
 print "arranca"
@@ -23,10 +24,57 @@ gdb = os.path.join(path, "LineasEMT_PRO.gdb")
 
 arcpy.env.workspace = os.path.join(gdb, "ds")
 
+#Backup de la GDB anterior
+hoy = time.strftime("%Y%m%d_%H%M%S")
+#print hoy
+# in_data = os.path.join(path, "LineasEMT_PRO.gdb")
+#print in_data
+out_data = os.path.join(path, "LineasEMT_PRO"+"_"+ hoy +".gdb")
+#print out_data
+# os.rename(in_data, out_data)
+arcpy.Copy_management(gdb, out_data)
+
 for fc in arcpy.ListFeatureClasses():
     if fc == "bus_lines" or fc == "bus_lines_elevation" or fc == "here_streets_bike_elevation":
         arcpy.DeleteFeatures_management(fc)
         print "La clase de entidad '" + fc + "' tiene " + str(arcpy.GetCount_management(fc)) + " entidades"
+    if fc == "here_streets_bike" or fc == "here_streets_walk":
+        print "Inicio delete " + str(datetime.now().time())
+
+        # Se crea este nuevo campo que numera todas las entidades de la fc a partir de 0, para no tener que utilizar ObjectID
+        # Este nuevo campo se eliminara despues de utilizarlo
+        arcpy.AddField_management(fc, "Aux", "DOUBLE")
+
+        edit = arcpy.da.Editor(gdb)
+        edit.startEditing(False, False)
+        edit.startOperation()
+
+        max = 0
+        j = 0
+        with arcpy.da.UpdateCursor(fc, 'Aux') as cursor:
+            for row in cursor:
+                row[0] = j
+                j += 1
+                if j > max:
+                    max = j
+                cursor.updateRow(row)
+
+        i = 100000
+        while int(arcpy.GetCount_management(fc).getOutput(0)) != 0:
+            arcpy.MakeFeatureLayer_management(fc, fc + "_layer", where_clause = ' "Aux" <= ' + str(i))
+            arcpy.DeleteFeatures_management(fc + "_layer")
+            i = i + 100000
+            if int(arcpy.GetCount_management(fc).getOutput(0)) < 100000:
+                i = max
+
+            print "Fin delete " + str(datetime.now().time())
+            print "La clase de entidad '" + fc + "' tiene " + str(arcpy.GetCount_management(fc).getOutput(0)) + " entidades"
+
+        edit.stopOperation()
+        edit.stopEditing(True)
+
+        arcpy.DeleteField_management(fc, "Aux")
+
 
 # xml = os.path.join(path, "Schema.xml")
 #
@@ -40,100 +88,100 @@ for fc in arcpy.ListFeatureClasses():
 #
 # # arcpy.ImportXMLWorkspaceDocument_management(gdbFINAL, xml, "SCHEMA_ONLY")
 
-#Importamos los datos a las nueva GDB
+Importamos los datos a las nueva GDB
 
-#INTERURBANO
-# interurbano = os.path.join(path, "LineasEMT_PRO.gdb\ds")
-# #Lineas
-# inputs=os.path.join(interurbano, "Interurbanos_Lineas")
-# target=os.path.join(gdb,"ds\Interurbanos_Lineas")
-# schema_type="NO_TEST"
-# arcpy.Append_management(inputs,target,schema_type)
-# #accesos
-# inputs=os.path.join(interurbano, "Interurbanos_Accesos")
-# target=os.path.join(gdb,"ds\Interurbanos_Accesos")
-# schema_type="NO_TEST"
-# arcpy.Append_management(inputs,target,schema_type)
-# #elevations
-# inputs=os.path.join(interurbano, "Interurbanos_Elevation")
-# target=os.path.join(gdb,"ds\Interurbanos_Elevation")
-# schema_type="NO_TEST"
-# arcpy.Append_management(inputs,target,schema_type)
-# #transbordos
-# inputs=os.path.join(interurbano, "Interurbanos_Transbordos")
-# target=os.path.join(gdb,"ds\Interurbanos_Transbordos")
-# schema_type="NO_TEST"
-# arcpy.Append_management(inputs,target,schema_type)
+INTERURBANO
+interurbano = os.path.join(path, "LineasEMT_PRO.gdb\ds")
+#Lineas
+inputs=os.path.join(interurbano, "Interurbanos_Lineas")
+target=os.path.join(gdb,"ds\Interurbanos_Lineas")
+schema_type="NO_TEST"
+arcpy.Append_management(inputs,target,schema_type)
+#accesos
+inputs=os.path.join(interurbano, "Interurbanos_Accesos")
+target=os.path.join(gdb,"ds\Interurbanos_Accesos")
+schema_type="NO_TEST"
+arcpy.Append_management(inputs,target,schema_type)
+#elevations
+inputs=os.path.join(interurbano, "Interurbanos_Elevation")
+target=os.path.join(gdb,"ds\Interurbanos_Elevation")
+schema_type="NO_TEST"
+arcpy.Append_management(inputs,target,schema_type)
+#transbordos
+inputs=os.path.join(interurbano, "Interurbanos_Transbordos")
+target=os.path.join(gdb,"ds\Interurbanos_Transbordos")
+schema_type="NO_TEST"
+arcpy.Append_management(inputs,target,schema_type)
 
 
-#METRO
-# metro = os.path.join(path, "LineasEMT_PRO.gdb\ds")
-# #Lineas
-# inputs=os.path.join(metro, "Metro_Lines")
-# target=os.path.join(gdb,"ds\Metro_Lines")
-# schema_type="NO_TEST"
-# arcpy.Append_management(inputs,target,schema_type)
-# #accesos
-# inputs=os.path.join(metro, "Metro_Accesos")
-# target=os.path.join(gdb,"ds\Metro_Accesos")
-# schema_type="NO_TEST"
-# arcpy.Append_management(inputs,target,schema_type)
-# #elevations
-# inputs=os.path.join(metro, "Metro_lines_elevation")
-# target=os.path.join(gdb,"ds\Metro_lines_elevation")
-# schema_type="NO_TEST"
-# arcpy.Append_management(inputs,target,schema_type)
-# #transbordos
-# inputs=os.path.join(metro, "Metro_transbordos")
-# target=os.path.join(gdb,"ds\Metro_transbordos")
-# schema_type="NO_TEST"
-# arcpy.Append_management(inputs,target,schema_type)
+METRO
+metro = os.path.join(path, "LineasEMT_PRO.gdb\ds")
+#Lineas
+inputs=os.path.join(metro, "Metro_Lines")
+target=os.path.join(gdb,"ds\Metro_Lines")
+schema_type="NO_TEST"
+arcpy.Append_management(inputs,target,schema_type)
+#accesos
+inputs=os.path.join(metro, "Metro_Accesos")
+target=os.path.join(gdb,"ds\Metro_Accesos")
+schema_type="NO_TEST"
+arcpy.Append_management(inputs,target,schema_type)
+#elevations
+inputs=os.path.join(metro, "Metro_lines_elevation")
+target=os.path.join(gdb,"ds\Metro_lines_elevation")
+schema_type="NO_TEST"
+arcpy.Append_management(inputs,target,schema_type)
+#transbordos
+inputs=os.path.join(metro, "Metro_transbordos")
+target=os.path.join(gdb,"ds\Metro_transbordos")
+schema_type="NO_TEST"
+arcpy.Append_management(inputs,target,schema_type)
 
-#METRO LIGERO
-# metro_ligero = os.path.join(path, "LineasEMT_PRO.gdb\ds")
-# #Lineas
-# inputs=os.path.join(metro_ligero, "Ligero_Lineas")
-# target=os.path.join(gdb,"ds\Ligero_Lineas")
-# schema_type="NO_TEST"
-# arcpy.Append_management(inputs,target,schema_type)
-# #accesos
-# inputs=os.path.join(metro_ligero, "Ligero_Accesos")
-# target=os.path.join(gdb,"ds\Ligero_Accesos")
-# schema_type="NO_TEST"
-# arcpy.Append_management(inputs,target,schema_type)
-# #elevations
-# inputs=os.path.join(metro_ligero, "Ligero_Elevations")
-# target=os.path.join(gdb,"ds\Ligero_Elevations")
-# schema_type="NO_TEST"
-# arcpy.Append_management(inputs,target,schema_type)
-# #transbordos
-# inputs=os.path.join(metro_ligero, "Ligero_Transbordos")
-# target=os.path.join(gdb,"ds\Ligero_Transbordos")
-# schema_type="NO_TEST"
-# arcpy.Append_management(inputs,target,schema_type)
+METRO LIGERO
+metro_ligero = os.path.join(path, "LineasEMT_PRO.gdb\ds")
+#Lineas
+inputs=os.path.join(metro_ligero, "Ligero_Lineas")
+target=os.path.join(gdb,"ds\Ligero_Lineas")
+schema_type="NO_TEST"
+arcpy.Append_management(inputs,target,schema_type)
+#accesos
+inputs=os.path.join(metro_ligero, "Ligero_Accesos")
+target=os.path.join(gdb,"ds\Ligero_Accesos")
+schema_type="NO_TEST"
+arcpy.Append_management(inputs,target,schema_type)
+#elevations
+inputs=os.path.join(metro_ligero, "Ligero_Elevations")
+target=os.path.join(gdb,"ds\Ligero_Elevations")
+schema_type="NO_TEST"
+arcpy.Append_management(inputs,target,schema_type)
+#transbordos
+inputs=os.path.join(metro_ligero, "Ligero_Transbordos")
+target=os.path.join(gdb,"ds\Ligero_Transbordos")
+schema_type="NO_TEST"
+arcpy.Append_management(inputs,target,schema_type)
 
-#CERCANIAS
-# cercanias = os.path.join(path, "LineasEMT_PRO.gdb\ds")
-# #Lineas
-# inputs=os.path.join(cercanias, "Cercanias_Lineas")
-# target=os.path.join(gdb,"ds\Cercanias_Lineas")
-# schema_type="NO_TEST"
-# arcpy.Append_management(inputs,target,schema_type)
-# #accesos
-# inputs=os.path.join(cercanias, "Cercanias_Accesos")
-# target=os.path.join(gdb,"ds\Cercanias_Accesos")
-# schema_type="NO_TEST"
-# arcpy.Append_management(inputs,target,schema_type)
-# #elevations
-# inputs=os.path.join(cercanias, "Cercanias_Elevations")
-# target=os.path.join(gdb,"ds\Cercanias_Elevations")
-# schema_type="NO_TEST"
-# arcpy.Append_management(inputs,target,schema_type)
-# #transbordos
-# inputs=os.path.join(cercanias, "Cercanias_Transbordos")
-# target=os.path.join(gdb,"ds\Cercanias_Transbordos")
-# schema_type="NO_TEST"
-# arcpy.Append_management(inputs,target,schema_type)
+CERCANIAS
+cercanias = os.path.join(path, "LineasEMT_PRO.gdb\ds")
+#Lineas
+inputs=os.path.join(cercanias, "Cercanias_Lineas")
+target=os.path.join(gdb,"ds\Cercanias_Lineas")
+schema_type="NO_TEST"
+arcpy.Append_management(inputs,target,schema_type)
+#accesos
+inputs=os.path.join(cercanias, "Cercanias_Accesos")
+target=os.path.join(gdb,"ds\Cercanias_Accesos")
+schema_type="NO_TEST"
+arcpy.Append_management(inputs,target,schema_type)
+#elevations
+inputs=os.path.join(cercanias, "Cercanias_Elevations")
+target=os.path.join(gdb,"ds\Cercanias_Elevations")
+schema_type="NO_TEST"
+arcpy.Append_management(inputs,target,schema_type)
+#transbordos
+inputs=os.path.join(cercanias, "Cercanias_Transbordos")
+target=os.path.join(gdb,"ds\Cercanias_Transbordos")
+schema_type="NO_TEST"
+arcpy.Append_management(inputs,target,schema_type)
 
 
 #De la GDB de EMT sacamos los datos de bus, bikes y walk
@@ -153,10 +201,10 @@ arcpy.Append_management(inputs,target,schema_type)
 
 #BIKES
 #lines
-# inputs=os.path.join(gdbEMT, "here_streets_bike")
-# target=os.path.join(gdb, "ds\here_streets_bike")
-# schema_type="NO_TEST"
-# arcpy.Append_management(inputs,target,schema_type)
+inputs=os.path.join(gdbEMT, "here_streets_bike")
+target=os.path.join(gdb, "ds\here_streets_bike")
+schema_type="NO_TEST"
+arcpy.Append_management(inputs,target,schema_type)
 ###elevations
 inputs=os.path.join(gdbEMT, "here_streets_bike_elevation")
 target=os.path.join(gdb, "ds\here_streets_bike_elevation")
@@ -165,10 +213,10 @@ arcpy.Append_management(inputs,target,schema_type)
 
 #WALK
 #lines
-# inputs=os.path.join(gdbEMT, "here_streets_walk")
-# target=os.path.join(gdb, "ds\here_streets_walk")
-# schema_type="NO_TEST"
-# arcpy.Append_management(inputs,target,schema_type)
+inputs=os.path.join(gdbEMT, "here_streets_walk")
+target=os.path.join(gdb, "ds\here_streets_walk")
+schema_type="NO_TEST"
+arcpy.Append_management(inputs,target,schema_type)
 
 # arcpy.Delete_management(xml)
 
